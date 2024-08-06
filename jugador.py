6592664187816b7fb0fa8551d3f95945e301ddd6
chenaoh <ariza.ps4@gmail.com>
2024-08-06 14:07:51 -0600

    Mostrar circulo de color

--- jugador.py
+++ jugador.py
@@ -7,3 +7,12 @@ maximo_intentos = 10
 
 code = random.choices(colores, k=code_length)
 intentos = 0
+
+def mostrar_colores(paleta):
+    colores_disponibles = {
+        "Red": "🔴",
+        "Green": "🟢",
+        "Yellow": "🟡",
+        "Blue": "🔵"
+    }
+    return " ".join(colores_disponibles[color] for color in paleta)
