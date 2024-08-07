208673980c155f8be972a76b690f40d0147d2a73
chenaoh <ariza.ps4@gmail.com>
2024-08-07 10:18:47 -0600

    Creacion de las class para el creador del codigo y para el adivinador del codigo

--- jugador.py
+++ jugador.py
@@ -6,3 +6,19 @@ class Jugador:
     def __init__(self, el_jugador = True):
         self.el_jugador = el_jugador
         
+class Creador(Jugador):
+    def hace_codigo(self):
+        if self.el_jugador:
+            color_codigo = input("Ingrese el codigo de colores: ").strip().split()
+        else:
+            color_codigo = random.choices(self.colores, k=4)
+        return color_codigo
+    
+    
+class Adivinar(Jugador):
+    def adivina_codigo(self):
+        if self.el_jugador:
+            color_codigo = input("Ingrese el codigo de colores a adivinar: ").strip().split()
+        else:
+            color_codigo = random.choices(self.colores, k=4)
+        return color_codigo
